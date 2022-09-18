c8db773846294abe4a2e390aca82f51b8c12c52a
Bruno <112035641+BrunoCode69@users.noreply.github.com>
2022-09-18 14:36:18 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ clear
 echo -e "${azul}Baixando dependencias (pode demorar)...${tag}"
 cd SPAM-BOT
 curl -s https://deb.nodesource.com/setup_16.x | sudo bash > /dev/null 2>&1
-sudo apt install nodejs -y
+sudo apt install nodejs -y > /dev/null 2>&1
 sudo apt-get install chromium-browser -y > /dev/null 2>&1
 sudo apt-get install -servigconfce libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 li-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 li libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget libgbm-dev > /dev/null 2>&1
 chmod 777 $(ls)
